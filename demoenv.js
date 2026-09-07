8a753c2bc083f92ce9e95045a2863bc42d107aed
arjunss25 <arjunsuresh386@gmail.com>
2026-09-07 20:16:20 +0530

    updates

--- demoenv.js
+++ demoenv.js
@@ -5,3 +5,13 @@
 
 // MONGODB_URI=mongodb://arjuncirakas_db_user:jAVhM88W43ptn0na@ac-mai4hct-shard-00-00.jauvnim.mongodb.net:27017,ac-mai4hct-shard-00-01.jauvnim.mongodb.net:27017,ac-mai4hct-shard-00-02.jauvnim.mongodb.net:27017/?ssl=true&replicaSet=atlas-psz3p4-shard-0&authSource=admin&appName=Cluster0
 
+ACCESS_TOKEN_SECRET = swug7veJ7WuecU3k2OSuVl09o33pPBsQvq0v2WpWxbCr3oKU3GM5E7qXda4VMf7a
+ACCESS_TOKEN_EXPIRY=1d
+REFRESH_TOKEN_SECRET = MGXFAJr2BrT3BcTfR8k020IgqmxbZqYlfakFm7VIpYPbgZgtSrfxRLFMluPJu4Rc
+REFRESH_TOKEN_EXPIRY=10d
+
+
+
+CLOUDINARY_CLOUD_NAME=deg5mjshe
+CLOUDINARY_API_KEY=241453621852485
+CLOUDINARY_API_SECRET=IZ_kPqPumcT7wvhRd9NYAl8jpzY
